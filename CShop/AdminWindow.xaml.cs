013bba785c918e032110d9ad2d454d295d13c945
Ondřej Brabec <brabe.ondrej@gmail.com>
2018-08-25 12:42:54 +0200

    Container

--- CShop/AdminWindow.xaml.cs
+++ CShop/AdminWindow.xaml.cs
@@ -10,27 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CShop
 {
     /// <summary>
-    /// Interakční logika pro MainWindow.xaml
+    /// Interakční logika pro AdminWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AdminWindow : Window
     {
-        public MainWindow()
+        public AdminWindow()
         {
             InitializeComponent();
         }
-
-        private void Admin_Open(object sender, RoutedEventArgs e)
-        {
-
-            AdminWindow aw = new AdminWindow();
-            aw.Show();
-        }
-        
     }
 }
---
+++ CShop/Container.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CShop
+{
+    class Container
+    {
+        interface INode
+        {
+            bool isIt(string name);
+        }
+        internal class BasicNode : INode
+        {
+            private string name;
+            public string Name { get => name; internal set => name = value; }
+            public bool isIt(string name)
+            {
+                return (this.Name == name);
+            }
+        }
+        private class Node<I> : BasicNode
+        {
+            
+            private I instance;
+
+    
+            public Node(string name, I instance)
+            {
+                Name = name;
+                Instance = instance;
+            }
+
+            public I Instance { get => instance; private set => instance = value; }
+           
+
+           
+        }
+
+        private List<BasicNode> nodes;
+        private static Container instance;
+        private static readonly object padlock = new object();
+
+        public static Container Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Container();
+                    }
+                    return instance;
+                };
+            }
+        }
+
+        private Container()
+        {
+            this.nodes = new List<BasicNode>();
+            Add<AdminWindow>("AdminWindow", new AdminWindow());
+        }
+
+        public void Add<I>(string name, I instance)
+        {
+                nodes.Add(new Node<I>(name, instance));            
+        }
+        
+        public I Get<I>(string name)
+        {
+            foreach (var node in nodes)
+            {
+                Node<I> maine = (Node<I>)node;
+                if(node.isIt(name))
+                {
+                    
+                    I instance1 = maine.Instance;
+                    return instance1;
+                }
+            }
+            return default(I);
+        }
+    }
+}
